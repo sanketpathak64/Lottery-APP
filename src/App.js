fc1b7e24933f22059af968fc73a846779699f67e
sanket pathak <sanketpathak64@gmail.com>
2020-03-12 04:05:20 +0530

    added user form

--- src/App.js
+++ src/App.js
@@ -6,18 +6,41 @@ import lottery from './lottery';
 class App extends Component {
 
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
   };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-
-    this.setState({manager});
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({manager,players,balance});
   }
   render(){
     return (
       <div>
         <h2>Lottery App</h2>
-        <p>This contract is managed by {this.state.manager} </p>
+        <p>This contract is managed by {this.state.manager}
+        Threre are currently {this.state.players.length} people entered to
+        win {web3.utils.fromWei(this.state.balance)} ether!</p>
+
+        <hr />
+        <form>
+          <h4>
+            Want to try your luck
+          </h4>
+          <div>
+            <label>
+              Amount of ether to enter 
+            </label>
+            <input 
+            value = {this.state.value}
+              onChange = {event => this.setState({value: event.target.value})}
+            />
+          </div>
+          <button>enter</button>
+        </form>
       </div>
     );
   }
